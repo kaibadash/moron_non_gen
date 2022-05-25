6102fba50eab29a12ab8f8626ef011eca6f82c3b
kaiba <kaibadash@gmail.com>
2022-05-26 01:34:08 +0900

    Add link about: #誰がもちのろんや

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ function App() {
       <h1>もろんのんっぽい文字列ジェネレータ</h1>
       <div id="result">{result}</div>
       <p>レーベンシュタイン距離(小さいほど正解に近い): {levenshtein.get(ANSWER, result)}</p>
-      <footer><a href="https://github.com/kaibadash/moron_non_gen">ソース</a></footer>
+      <footer>
+        <a href="https://twitter.com/hashtag/%E8%AA%B0%E3%81%8C%E3%82%82%E3%81%A1%E3%81%AE%E3%82%8D%E3%82%93%E3%82%84?src=hashtag_click">#誰がもちのろんや</a><br />
+        <a href="https://github.com/kaibadash/moron_non_gen">ソース</a>
+      </footer>
     </div>
   );
 }
